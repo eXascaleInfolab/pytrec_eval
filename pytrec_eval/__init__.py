6e57db76fb4884f4fae5d854ae744082d289dc09
Alberto Tonon <alberto.tonon@exascale.info>
2014-02-12 14:02:29 +0100

    pygal plots

--- pytrec_eval/__init__.py
+++ pytrec_eval/__init__.py
@@ -4,4 +4,4 @@ from pytrec_eval.evaluation_core import *
 from pytrec_eval.metrics import *
 from pytrec_eval.utils import *
 from pytrec_eval.ranking import *
-from pytrec_eval.plots import *
+from pytrec_eval.pygal_plots import *
--- pytrec_eval/metrics.py
+++ pytrec_eval/metrics.py
@@ -9,33 +9,35 @@ __author__ = 'alberto'
 # details is a dictionary details[topicID] = score is returned.
 
 
-def precision(run, qrels, detailed = False):
+def precision(run, qrels, detailed=False):
     """Computes average precision among all entities."""
     details = {}
     avg = 0
     for topicId, entryList in run.entries.items():
-        numRelevant = len([ docId for docId, score, _ in entryList
-                            if qrels.isRelevant(topicId, docId) ])
+        numRelevant = len([docId for docId, score, _ in entryList
+                           if qrels.isRelevant(topicId, docId)])
         numReturned = len(entryList)
         details[topicId] = numRelevant / numReturned
         avg += numRelevant / numReturned
     numTopics = qrels.getNTopics()
     return avg / numTopics if not detailed else (avg / numTopics, details)
 
-def recall(run, qrels, detailed = False):
+
+def recall(run, qrels, detailed=False):
     """Computes recall"""
     details = {}
     avg = 0
     for topicId, entryList in run.entries.items():
-        numRelevantFound = len([ docId for docId, score, _ in entryList
-                                 if qrels.isRelevant(topicId, docId) ])
+        numRelevantFound = len([docId for docId, score, _ in entryList
+                                if qrels.isRelevant(topicId, docId)])
         numRelevant = qrels.getNRelevant(topicId)
         details[topicId] = numRelevantFound / numRelevant
         avg += numRelevantFound / numRelevant
     numtopics = qrels.getNTopics()
     return avg / numtopics if not detailed else (avg / numtopics, details)
 
-def avgPrec(run, qrels, detailed = False):
+
+def avgPrec(run, qrels, detailed=False):
     """Computes average precision."""
     details = {}
     avg = 0
@@ -53,25 +55,29 @@ def avgPrec(run, qrels, detailed = False):
     numtopics = qrels.getNTopics()
     return avg / numtopics if not detailed else (avg / numtopics, details)
 
+
 def precisionAt(rank):
     """
     Computes precision@rank. Returns a function that,
     satisfies the requirements defined on the top of this module and that
     computes precision@rank.
     """
-    def precisionAtRank(run, qrels, detailed = False):
+
+    def precisionAtRank(run, qrels, detailed=False):
         details = {}
         avg = 0
         for topicId, entryList in run.entries.items():
-            numRelevant = len([ docId for docId, score, _ in entryList[0 : rank]
-                                if qrels.isRelevant(topicId, docId) ])
+            numRelevant = len([docId for docId, score, _ in entryList[0: rank]
+                               if qrels.isRelevant(topicId, docId)])
             details[topicId] = numRelevant / rank
             avg += numRelevant / rank
         numtopics = qrels.getNTopics()
         return avg / numtopics if not detailed else (avg / numtopics, details)
+
     return precisionAtRank
 
-def ndcg(run, qrels, detailed = False):
+
+def ndcg(run, qrels, detailed=False):
     """
     Computes NDCG using the formula
     DCG_p = rel_1 + \sum_{i = 2}^p( rel_i / log_2(i) )
@@ -82,28 +88,31 @@ def ndcg(run, qrels, detailed = False):
     details = {}
     avg = 0
     for topicId, entryList in run.entries.items():
-        relevancesByRank = qrels.getRelevanceScores( topicId, [doc for (doc, _, _) in entryList] )
-        sumdcg = relevancesByRank[0] + sum( [ relScore / math.log2(rank)
-                                              for rank, relScore in enumerate(relevancesByRank[1:], start=2)] )
+        relevancesByRank = qrels.getRelevanceScores(topicId, [doc for (doc, _, _) in entryList])
+        sumdcg = relevancesByRank[0] + sum([relScore / math.log2(rank)
+                                            for rank, relScore in enumerate(relevancesByRank[1:], start=2)])
         #sumdcg = sum( [ (2**relScore - 1) / math.log2(rank+1)
         #                for rank, relScore in enumerate(relevancesByRank, start=1)] )
-        relevancesByRank.sort(reverse = True) # sort the relevance list descending order
-        sumIdcg = relevancesByRank[0] + sum( [ relScore / math.log2(rank)
-                                               for rank, relScore in enumerate(relevancesByRank[1:], start=2)] )
+        relevancesByRank.sort(reverse=True)  # sort the relevance list descending order
+        sumIdcg = relevancesByRank[0] + sum([relScore / math.log2(rank)
+                                             for rank, relScore in enumerate(relevancesByRank[1:], start=2)])
         #sumIdcg = sum( [ (2**relScore - 1) / math.log2(rank+1)
         #                   for rank, relScore in enumerate(relevancesByRank, start=1)] )
-        if sumIdcg == 0: print(topicId, relevancesByRank)
-        details[topicId] = sumdcg / sumIdcg
+        if sumIdcg == 0:
+            details[topicId] = sumdcg / sumIdcg
+        else:
+            details[topicId] = sumdcg / sumIdcg
         avg += sumdcg / sumIdcg
     numtopics = qrels.getNTopics()
     return avg / numtopics if not detailed else (avg / numtopics, details)
 
+
 STD_METRICS = [avgPrec, ndcg]
 
 METRICS_NAMES = {
-    precision : 'Precision',
-    ndcg : 'NDCG',
-    precisionAt : 'Precision@k',
+    precision: 'Precision',
+    ndcg: 'NDCG',
+    precisionAt: 'Precision@k',
     recall: 'recall',
     avgPrec: 'Average Precision'
 }
