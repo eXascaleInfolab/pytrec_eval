8e65b67acd6aa51eafa4fe5dff8ab29757752239
Alberto Tonon <alberto.tonon@exascale.info>
2016-05-11 16:41:41 +0200

    removed pygal

--- pytrec_eval/__init__.py
+++ pytrec_eval/__init__.py
@@ -5,7 +5,6 @@ from pytrec_eval.qrels import *
 from pytrec_eval.metrics import *
 from pytrec_eval.utils import *
 from pytrec_eval.ranking import *
-from pytrec_eval.pygal_plots import *
 from pytrec_eval.pandas_utils import *
 from pytrec_eval.classification_metrics import *
 from pytrec_eval.clustering_metrics import *
